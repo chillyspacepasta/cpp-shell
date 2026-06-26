cb698654aed33edd8235f127715539b5d96f25aa
chinmay <chinmaykulkarni758@gmail.com>
2026-06-26 22:28:42 +0530

    repl

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <limits>
+#include <string>
 
 int main()
-{
-std::cout << "hi, i am a cpp shell!" << std::endl;
-return 0; 
-}
+    {
+    std::string input;
 
+    while (1)
+        {
+        std::cout << "cc_shell> ";
+
+        if (!std::getline(std::cin, input))
+            {
+            std::cout << "\ninput stream requested to be closed, exiting...";
+            return 1;
+            }
+
+        else if (input == "exit")
+            {
+            std::cout << "exiting...";
+            break;
+            }
+
+        std::cout << "[Echo]: " << input << std::endl;
+        }
+
+    return 0;
+
+    }
